1b4687a0239a62f8f347f1962fa3d4e39ab59bd1
junya-yachi <109054387+junya-yachi@users.noreply.github.com>
2023-01-31 17:03:19 +0900

    add: users>controllers, update: routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/resistrations'
+  }
+  devise_scope :user do
+    post 'users/sign_up/confirm',  to: 'users/registrstions#confirm'
+    get  'users/sign_up/complete', to: 'users/registrations#complete'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
