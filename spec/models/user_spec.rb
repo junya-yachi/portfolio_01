bc665e79a601df4e07c4485849aae87bbc886992
junya-yachi <109054387+junya-yachi@users.noreply.github.com>
2023-02-05 16:04:11 +0900

    add:devise:i18n, update:modelspec
    devise:i18n導入。modelスペック記述(途中)。

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe 'User', type: :model do
   describe 'ユーザー登録のテスト' do
     let(:user) { create(:user)}
+    let(:user2) { create(:user)}
 
     context "登録が完了出来る場合のテスト" do
       it '登録完了(正常)' do
@@ -14,20 +15,30 @@ RSpec.describe 'User', type: :model do
       it 'nameが未入力でエラー' do
         user.name = ''
         user.valid?
-        expect(user.errors.full_messages).to include() 
+        # expect(user.errors.full_messages).to be_added(:name, :blank)
+        expect(user.errors).to be_of_kind(:name, :blank)
       end
-    
       it 'emailが未入力でエラー' do
         user.email = ''
         user.valid?
-        expect(user.errors.full_messages).to include() 
+        expect(user.errors).to be_of_kind(:email, :blank)
       end
-
       it 'passwordが未入力でエラー' do
         user.password = ''
         user.valid?
-        expect(user.errors.full_messages).to include() 
+        expect(user.errors).to be_of_kind(:password, :blank)
       end
+      it '256文字以上(上限オーバー)のメールアドレスでエラー' do
+        # user.email = 'a' * 246 + '@sample.jp'
+        user.email = Faker::Lorem.characters(246) + '@sample.jp'
+        user.valid?
+        expect(user.errors).to be_added(:email, :too_long, count: 255)
+      end
+      # it '重複したメールアドレスが存在する場合は登録出来ない' do
+      #   user.save
+      #   user2.email = user.email
+      #   expect(subject.error).to to be_of_kind(:email, :already_confirmed)
+      # end
     end
   end
 end
