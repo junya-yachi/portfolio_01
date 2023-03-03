460c55d20bdbc827c3c33787a6b80c63ffe185a7
junya-yachi <109054387+junya-yachi@users.noreply.github.com>
2023-03-03 17:02:09 +0900

    update:top.html

--- app/javascript/custom/top.js
+++ app/javascript/custom/top.js
@@ -1,7 +1,18 @@
-$(function(){
-  $("window").on("load", function(){
+// $(function(){
+//   $("window").on("load", function(){
+//     $(".main_item_3").css({
+//       "font": "#fff"
+//     });
+//   });
+// });
+$(function() {
+  $("#button").on("click", function() {
     $(".main_item_3").animate({
-      "marginleft": "100px"
+      "top": "100px"
     });
   });
 });
+
+// $(function () {
+//   $(".main_item_3").css("color", "#0000FF");
+// }); //jqeuryは成功している
