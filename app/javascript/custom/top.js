fa7254e1094539530be6e0036b6c5616e886d49b
junya-yachi <109054387+junya-yachi@users.noreply.github.com>
2023-03-07 17:08:00 +0900

    update:top.html

--- app/javascript/custom/top.js
+++ app/javascript/custom/top.js
@@ -5,13 +5,18 @@
 //     });
 //   });
 // });
-$(function() {
-  $("#button").on("click", function() {
-    $(".main_item_3").animate({
-      "top": "100px"
-    });
-  });
-});
+// $(function() {
+//   $("#button").on("click", function() {
+//     $(".main_item_3").animate({
+//       "top": "100px"
+//     });
+//   });
+//   $('.fadeInUpTriggerOnce').on('inview', function(event, isInView) {
+//     if (isInView) {//表示領域に入った時
+//       $(this).addClass('animate__animated animate__fadeInUp');//クラス名が付与
+//     }
+//   });
+// });
 
 // $(function () {
 //   $(".main_item_3").css("color", "#0000FF");
