d333588877ce92ec47b80dbc9014f6c23e58d2fb
junya-yachi <109054387+junya-yachi@users.noreply.github.com>
2023-03-09 17:00:46 +0900

    cm

--- app/javascript/custom/top.js
+++ app/javascript/custom/top.js
@@ -1,22 +1,12 @@
-// $(function(){
-//   $("window").on("load", function(){
-//     $(".main_item_3").css({
-//       "font": "#fff"
-//     });
-//   });
-// });
-// $(function() {
-//   $("#button").on("click", function() {
-//     $(".main_item_3").animate({
-//       "top": "100px"
-//     });
-//   });
-//   $('.fadeInUpTriggerOnce').on('inview', function(event, isInView) {
-//     if (isInView) {//表示領域に入った時
-//       $(this).addClass('animate__animated animate__fadeInUp');//クラス名が付与
-//     }
-//   });
-// });
+$(function(){
+  $('.fadeInUpTrigger').on('inview', function(event, isInView) {
+    if (isInView) {//表示領域に入った時
+      $(this).addClass('animate__animated animate__fadeInUp');//クラス名が付与
+    } else {//表示領域から出た時
+      $(this).removeClass('animate__animated animate__fadeInUp');//クラス名が除去
+    }
+  });
+});
 
 // $(function () {
 //   $(".main_item_3").css("color", "#0000FF");
