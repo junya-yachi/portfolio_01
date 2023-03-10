610baf2a3119e3bae6daa77a81669991c85edcda
junya-yachi <109054387+junya-yachi@users.noreply.github.com>
2023-03-10 17:17:27 +0900

    update:top

--- app/javascript/custom/top.js
+++ app/javascript/custom/top.js
@@ -1,4 +1,4 @@
-$(function(){
+// $(function(){
   $('.fadeInUpTrigger').on('inview', function(event, isInView) {
     if (isInView) {//表示領域に入った時
       $(this).addClass('animate__animated animate__fadeInUp');//クラス名が付与
@@ -6,7 +6,7 @@ $(function(){
       $(this).removeClass('animate__animated animate__fadeInUp');//クラス名が除去
     }
   });
-});
+// });
 
 // $(function () {
 //   $(".main_item_3").css("color", "#0000FF");
