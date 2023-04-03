7d939d82f28a2211115af3111f3a420037763fdb
junya-yachi <109054387+junya-yachi@users.noreply.github.com>
2023-04-03 14:48:34 +0900

    update:client
    clientモデル修正。

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,6 @@
 class Client < ApplicationRecord
   belongs_to :user
   validates  :cilent_name,  presence: true
-  validates  :phone_num,    presence: true
-  validates  :client_email, presence: true
+  validates  :phone_num,    length: { maximum: 20 }
+  validates  :client_email, length: { maximum: 256 }
 end
