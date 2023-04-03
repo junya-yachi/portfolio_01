e2cee0c8a851d06c4573a93cc9bdcef9e5668636
junya-yachi <109054387+junya-yachi@users.noreply.github.com>
2023-04-03 16:01:37 +0900

    update:client
    clientモデル修正

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -10,12 +10,14 @@ class ClientsController < ApplicationController
     if params[:back] || !@client.save 
       render :new and return
     redirect_to root_path
+    elsif @client.save
+      flash[:notice] = "取引先を新規登録しました"
+      redirect_to clients_path
     end
   end
   def confirm
     @client = Client.new(client_params)
     if @client.invalid?
-      flash.now[:notice] = "入力内容にエラーがあります"
       render :new
       return
     end
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,13 @@
 class Client < ApplicationRecord
   belongs_to :user
   validates  :cilent_name,  presence: true
-  validates  :phone_num,    length: { maximum: 20 }
-  validates  :client_email, length: { maximum: 256 }
+  validates  :phone_num,    presence: true,
+                            length: { maximum: 20 },
+                            numericality: { only_integer: true}
+  validates  :client_email, presence: true,
+                            length: { maximum: 256 }
+  def phone_num=(value)
+    value.tr!('０-９', '0-9') if value.is_a?(String)
+    super(value)
+  end
 end
