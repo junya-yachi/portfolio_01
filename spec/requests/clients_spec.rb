90602efdc41a4920e06e6616fb58ee7c67abb22d
junya-yachi <109054387+junya-yachi@users.noreply.github.com>
2023-04-05 14:50:39 +0900

    update:clients::request_spec
    clientsのrequestスペック修正。

--- spec/requests/clients_spec.rb
+++ spec/requests/clients_spec.rb
@@ -1,15 +1,23 @@
 require 'rails_helper'
 RSpec.describe "Clients", type: :request do
+  let(:user) { create(:user) }
+  before do
+    sign_in user
+  end
   describe "GET /index" do
-    it "returns http success" do
-      get clients_path
-      expect(response).to have_http_status(200)
+    context "GET /clientsの場合" do
+      it "成功すること" do
+        get clients_path
+        expect(response).to have_http_status(200)
+      end
     end
   end
   describe "GET /new" do
-    it "returns http success" do
-      get new_client_path
-      expect(response).to have_http_status(200)
+    context "GET /clients/newの場合" do
+      it "成功すること" do
+        get new_client_path
+        expect(response).to have_http_status(200)
+      end
     end
   end
 
