322523c44be8570ab64fbf66cf005a8ec31c8de4
junya-yachi <109054387+junya-yachi@users.noreply.github.com>
2023-04-23 17:52:52 +0900

    update:top_spec
    top_spec修正。

--- spec/system/top_spec.rb
+++ spec/system/top_spec.rb
@@ -1,6 +1,34 @@
 require 'rails_helper'
 RSpec.describe "Homes", type: :system do
   describe 'ホーム画面のテスト' do
-    
+    let(:user) { create(:user) }
+
+    context "ユーザー未登録の場合" do
+      before do
+        visit homes_top_path
+      end
+      it "画面に新規登録が表示されていること" do
+        expect(page).to have_css '.top_right'
+      end
+      it "新規登録画面へ移動すること" do
+        click_link '新規登録'
+        expect(page).to have_css '.user_form'
+      end
+    end
+
+    context "ユーザー登録済みの場合" do
+      before do
+        login(user)
+        visit homes_top_path
+      end
+
+      it "画面にログインユーザーの名前が表示されていること" do
+        expect(page).to have_selector 'div', text: "#{user.name}"
+      end
+      it "在庫管理で商品一覧ページへ移動すること" do
+        click_link '在庫管理'
+        expect(page).to have_selector 'h1', text: "商品一覧"
+      end
+    end
   end
 end
